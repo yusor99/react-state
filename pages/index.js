d04ad9757a94a55fbac241457c48577cba039f30
yusor99 <yusorahmad@gmail.com>
2020-09-19 12:45:06 +0300

    2commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { Button, Row, Col, Input } from "antd";
+import { FolderAddOutlined, DeleteOutlined } from "@ant-design/icons";
 const Home = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
@@ -7,25 +8,63 @@ const Home = () => {
     setTodo(e.target.value);
   };
   const addNew = () => {
-    setTodos([ ...todos, todo ]);
-    setTodo("");
-  };
+    if(todo!=""){setTodos([...todos, todo]);
+      setTodo("");
+      localStorage.setItem("todos", JSON.stringify([...todos, todo]));
+    }
+    };
+  useEffect(() => {
+    const _todos = localStorage.getItem("todos");
+    if (_todos) {
+      setTodos(JSON.parse(_todos));
+    }
+  }, []);
+
   return (
     <div className="container">
       <div className="form">
-        <input placeholder="write here" value={todo} onChange={handelChange} />
-        <button onClick={addNew}>Add</button>
+        <Row gutter={20}>
+          <Col span={20}>
+            <Input
+              placeholder="write here"
+              value={todo}
+              onChange={handelChange}
+            />
+          </Col>
+          <Col span={4}>
+            <Button type="primary" onClick={addNew} style={{ width: "100%" }}>
+              Add <FolderAddOutlined />
+            </Button>
+          </Col>
+          
+        </Row>
       </div>
       <div>
         <div className="list">
           {todos.map((item, index) => (
             <div className="list-item" key={index}>
-              <p>{item}</p>
-              <button
-                onClick={() => setTodos(todos.filter((el) => el !== item))}
-              >
-                Remove
-              </button>
+              <Row gutter={20}>
+                <Col span={21}>
+                  <p>{item}</p>
+                </Col>
+                <Col span={3}>
+                  <Button  danger
+                  type="primary"
+                  style={{ marginTop:10}}
+                  shape="circle"><DeleteOutlined
+                 
+                    onClick={() => {
+                      setTodos(todos.filter((el) => el !== item));
+                      localStorage.setItem(
+                        "todos",
+                        JSON.stringify(todos.filter((el) => el !== item))
+                      );
+                      
+                    }}
+                  />
+                  </Button>
+                </Col>
+              </Row>
             </div>
           ))}
         </div>
